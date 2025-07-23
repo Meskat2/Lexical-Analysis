b3ce41b54d5d4795d38322470f070218079e1ca6
Meskat2 <meskath09@gmail.com>
2025-07-23 16:53:06 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,16 +4,23 @@
 using namespace std;
 
 int main() {
-    char input[100];
-    printf("Enter an expression: ");
-    scanf("%s", input);
+    char input[200];
+    printf("Enter a line: ");
+    fgets(input, sizeof(input), stdin);
 
-    printf("Operators found:\n");
-    for (int i = 0; input[i] != '\0'; i++) {
-        if (input[i] == '+' || input[i] == '-' || input[i] == '*' ||
-            input[i] == '/' || input[i] == '%' || input[i] == '=') {
-            printf("Operator: %c\n", input[i]);
+    if (input[0] == '/' && input[1] == '/') {
+        printf("Single line comment\n");
+    }
+    else if (input[0] == '/' && input[1] == '*') {
+        int len = strlen(input);
+        if (input[len - 3] == '*' && input[len - 2] == '/') {
+            printf("Multi-line comment\n");
+        } else {
+            printf("Possibly part of a multi-line comment\n");
         }
+    }
+    else {
+        printf("Not a comment\n");
     }
 
     return 0;
